7d310b7c70a325d0fba98a74f457debf6832a57d
X0lotl <danil200319771@gmail.com>
2022-11-07 13:20:18 +0200

    add bubble sort

--- lab6/sort.cpp
+++ lab6/sort.cpp
@@ -6,25 +6,50 @@
 #include <iostream>
 #include <map>
 
+void swap(int *xp, int *yp) {
+	int temp = *xp;
+	*xp = *yp;
+	*yp = temp;
+}
+
+void bubbleSort(int *arrayPtr, int n) {
+	int i, j;
+	for (i = 0; i < n - 1; i++) {
+		for (j = 0; j < n - i - 1; j++) {
+			if (arrayPtr[j] > arrayPtr[j + 1])
+				swap(&arrayPtr[j], &arrayPtr[j + 1]);
+		}
+	}
+
+}
+
 
 int *generateRandomArray() {
 	static int randomArray[100];
 
 	for (int i = 0; i < 100; i++) {
-		randomArray[i] = (int) random();
+		randomArray[i] = (int) random() % 100;
 	}
 
 	return randomArray;
 }
-
-std::string arrayToString();
 
 void testSortArray() {
 	int *randomArrayPtr;
 
 	randomArrayPtr = generateRandomArray();
 
-	for (int i = 0; i < 100 ; i++) {
+	std::map<int, int> randomArrayInMap;
+
+	for (int i = 0; i < 100; i++) {
+//		randomArrayInMap.insert(std::pair(i, randomArrayPtr[i]));
+		std::cout << i << " : " << randomArrayPtr[i] << std::endl;
+	}
+
+	std::cout << "Sorted array \n \n";
+
+	bubbleSort(randomArrayPtr, 100);
+	for (int i = 0; i < 100; i++) {
 		std::cout << i << " : " << randomArrayPtr[i] << std::endl;
 	}
 }
